d76891216233dc43eb46fc0af391facd96572188
S3fflexDev <168296160+S3fflexDev@users.noreply.github.com>
2025-08-10 02:13:10 +0200

    Update Credits.tsx

--- src/pages/Credits.tsx
+++ src/pages/Credits.tsx
@@ -9,7 +9,7 @@ function Credits() {
         <div style={{ height: 25 }}></div>
         <div className="main" style={{ gap: 15 }}>
           <h1 style={{ fontWeight: 600, fontSize: 42, textAlign: "center" }}>
-            Desarrolladores
+            Devs
           </h1>
           <div style={{ display: "flex", gap: 7.5, flexDirection: "column" }}>
             <a className="profile">S3fflexDev</a>
